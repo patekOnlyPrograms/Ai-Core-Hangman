f6e1339ac1f1cd0d873939b5bc7858eac5891115
Susruth <abhirama99@live.com>
2023-10-23 13:09:16 +0100

    Made simplified if statement

--- hangman/milestone_2.py
+++ hangman/milestone_2.py
@@ -6,7 +6,7 @@ choice = random.choice(word_list)
 word = choice
 print(word)
 guess = input("Enter a singe letter to guess the word \n")
-if len(guess) == 1 and ((guess >= 'a' and guess <= 'z') or (guess >= 'A' and guess <= 'Z')):
+if len(guess) == 1 and guess.isalpha():
     print("Good guess")
 else:
     print("Oops! That is not a valid input.")
