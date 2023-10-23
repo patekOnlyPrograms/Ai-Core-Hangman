777d520727faac7040285982d0a0a5577e03b828
Susruth <abhirama99@live.com>
2023-10-23 16:55:16 +0100

    Created milestone 3 with functions

--- hangman/milestone_3.py
+++ hangman/milestone_3.py
@@ -10,17 +10,17 @@ print(word)
 
 def check_guess(guess):
     lowerCaseguess = guess.lower()
-
+    if len(lowerCaseguess) == 1 and lowerCaseguess.isalpha():
+        if lowerCaseguess in word:
+            print(f"Good guess {lowerCaseguess} is in the word")
+        else:
+            print(f"Sorry, {lowerCaseguess} is not in the word, try again")
+    else:
+        print("Invalid letter. Please, enter a single alphabetical character.")
 
 def ask_for_input():
     while True:
         guess = input("Enter a singe letter to guess the word \n")
         check_guess(guess)
-        if len(guess) == 1 and guess.isalpha():
-            print("Good guess")
-            if guess in word:
-                print(f"Good guess {guess} is in the word")
-            else:
-                print(f"Sorry, {guess} is not in the word, try again")
-        else:
-            print("Invalid letter. Please, enter a single alphabetical character.")
+
+ask_for_input()
