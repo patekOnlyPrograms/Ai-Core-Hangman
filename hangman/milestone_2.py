6ca86a0c4788e18fe5a78303eeb81a851cfc26ff
Susruth <abhirama99@live.com>
2023-10-23 12:44:57 +0100

    Fixing PEP8 styling tips

--- hangman/milestone_2.py
+++ hangman/milestone_2.py
@@ -1,11 +1,12 @@
 import random
+
 word_list = ["apple", "banana", "cherry", "date", "elderberry"]
 print(word_list)
 choice = random.choice(word_list)
 word = choice
 print(word)
 guess = input("Enter a singe letter to guess the word \n")
-if len(guess) == 1 and ((guess >='a' and guess <= 'z') or (guess>='A' and guess<='Z')):
+if len(guess) == 1 and ((guess >= 'a' and guess <= 'z') or (guess >= 'A' and guess <= 'Z')):
     print("Good guess")
 else:
     print("Oops! That is not a valid input.")
