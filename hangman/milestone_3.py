dc3966f2f83cad6b28a6558907507876f450dce2
Susruth <abhirama99@live.com>
2023-10-23 15:19:15 +0100

    trying to get task 3 done

--- hangman/milestone_3.py
+++ hangman/milestone_3.py
@@ -5,13 +5,22 @@ print(word_list)
 choice = random.choice(word_list)
 word = choice
 print(word)
-while True:
-    guess = input("Enter a singe letter to guess the word \n")
-    if len(guess) == 1 and guess.isalpha():
-        print("Good guess")
-        if guess in word:
-            print(f"Good guess {guess} is in the word")
+
+
+
+def check_guess(guess):
+    lowerCaseguess = guess.lower()
+
+
+def ask_for_input():
+    while True:
+        guess = input("Enter a singe letter to guess the word \n")
+        check_guess(guess)
+        if len(guess) == 1 and guess.isalpha():
+            print("Good guess")
+            if guess in word:
+                print(f"Good guess {guess} is in the word")
+            else:
+                print(f"Sorry, {guess} is not in the word, try again")
         else:
-            print(f"Sorry, {guess} is not in the word, try again")
-    else:
-        print("Invalid letter. Please, enter a single alphabetical character.")
+            print("Invalid letter. Please, enter a single alphabetical character.")
