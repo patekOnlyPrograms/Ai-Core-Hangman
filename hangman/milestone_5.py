e22f5264cdcbc0b3831109714a3685c71f8a7d5e
Susruth <abhirama99@live.com>
2023-10-24 13:41:02 +0100

    Adds milestone 5 fixed odd behavior now dealing with lives not showing

--- hangman/milestone_5.py
+++ hangman/milestone_5.py
@@ -43,10 +43,10 @@ class Hangman:
                 if self.word[i] == guess:
                     self.word_guessed[i] = guess
                     print(self.word_guessed)
-                else:
-                    self.num_letters -= 1
-                    print(f"Sorry, {letter} is not in the word.")
-                    print(f"You have {self.num_lives} left")
+        else:
+            self.num_letters -= 1
+            print(f"Sorry, {guess} is not in the word.")
+            print(f"You have {self.num_lives} left")
 
     def ask_for_input(self):
         while True:
