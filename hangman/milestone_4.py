eabd6cbe0cc23432d853b3494b4891739a818504
Susruth <abhirama99@live.com>
2023-10-30 11:37:16 +0000

    Fixes erros where wrong letters would be shown instead of correct letters

--- hangman/milestone_4.py
+++ hangman/milestone_4.py
@@ -32,21 +32,22 @@ class Hangman:
         self.num_lives = num_lives
         self.word = random.choice(word_list)
         self.word_guessed = ["_" for _ in range((len(self.word)))]
-        self.num_letters = len(self.word)
+        self.num_letters = len(set(self.word))
         self.list_of_guesses = []
 
     def check_guess(self, guess):
         guess = guess.lower()
         if guess in self.word:
             print(f"Good guess {guess} is in the word")
+            self.num_letters -= 1
             for i, letter in enumerate(self.word):
                 if self.word[i] == guess:
                     self.word_guessed[i] = guess
-                    print(self.word_guessed)
-                else:
-                    self.num_letters -= 1
-                    print(f"Sorry, {letter} is not in the word.")
-                    print(f"You have {self.num_lives} left")
+            print(self.word_guessed)
+        else:
+            self.num_lives -= 1
+            print(f"Sorry, {guess} is not in the word.")
+            print(f"You have {self.num_lives} lives left.")
 
     def ask_for_input(self):
         while True:
