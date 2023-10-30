21a7496b32a84673f4fa1c9fd42e2a5aadba8a5f
Susruth <abhirama99@live.com>
2023-10-30 11:38:43 +0000

    Removes s unnecessary variable assignment

--- hangman/milestone_2.py
+++ hangman/milestone_2.py
@@ -3,8 +3,7 @@ import random
 word_list = ["apple", "banana", "cherry", "date", "elderberry"]
 print(word_list)
 choice = random.choice(word_list)
-word = choice
-print(word)
+print(choice)
 guess = input("Enter a singe letter to guess the word \n")
 if len(guess) == 1 and guess.isalpha():
     print("Good guess")
